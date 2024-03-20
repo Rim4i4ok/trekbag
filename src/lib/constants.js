270b8896baae6d4133048bf215ce95863bc4f7d5
Rim4i4ok <rim4i4ok@gmail.com>
2024-03-20 23:07:52 +0100

    extracting constants

---
+++ src/lib/constants.js
@@ -0,0 +1,24 @@
+export const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
+
+export const initialItems = [
+  {
+    id: 1,
+    name: "hello world",
+    packed: true,
+  },
+  {
+    id: 2,
+    name: "money",
+    packed: false,
+  },
+  {
+    id: 3,
+    name: "shower",
+    packed: false,
+  },
+];
