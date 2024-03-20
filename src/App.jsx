366e8ba02cd492fc5798f5ca738f5f9796cdcbc3
Rim4i4ok <rim4i4ok@gmail.com>
2024-03-20 22:30:03 +0100

    adding header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import BackgroundHeading from "./components/BackgroundHeading";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import ItemList from "./components/ItemList";
 import Sidebar from "./components/Sidebar";
 
