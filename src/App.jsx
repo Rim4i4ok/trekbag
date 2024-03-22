20763189cf0e4514e288baf38a3b3930177defad
Roman Miasnikov <roman_miasnikov@epam.com>
2024-03-22 12:26:33 +0100

    moving logic for adding into app

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,25 @@ import { initialItems } from "./lib/constants";
 
 function App() {
   const [items, setItems] = useState(initialItems);
+
+  const handleAddItem = (newItemText) => {
+    const newItem = {
+      id: new Date().getTime(),
+      name: newItemText,
+      package: false,
+    };
+
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
+
   return (
     <>
       <BackgroundHeading />
       <main>
         <Header />
         <ItemList items={items} />
-        <Sidebar setItems={setItems} />
+        <Sidebar handleAddItem={handleAddItem} />
       </main>
       <Footer />
     </>
