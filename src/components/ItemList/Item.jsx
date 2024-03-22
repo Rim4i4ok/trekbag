957782b5926d1d916063a38f94bc0f1979689b7f
Roman Miasnikov <roman_miasnikov@epam.com>
2024-03-22 15:34:06 +0100

    addong handlers for item on the list

--- src/components/ItemList/Item.jsx
+++ src/components/ItemList/Item.jsx
@@ -1,10 +1,15 @@
-export default function Item({ item }) {
+export default function Item({ item, handleDeleteItem, handleToggleItem }) {
   return (
     <li className="item">
       <label>
-        <input type="checkbox" checked={item.package} /> {item?.name}
+        <input
+          type="checkbox"
+          checked={item.package}
+          onChange={() => handleToggleItem(item.id)}
+        />{" "}
+        {item?.name}
       </label>
-      <button>❌</button>
+      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,10 +1,21 @@
 import Item from "./Item";
 
-export default function ItemList({ items }) {
+export default function ItemList({
+  items,
+  handleDeleteItem,
+  handleToggleItem,
+}) {
   return (
     <ul>
       {items.map((item) => {
-        return <Item key={item.id} item={item} />;
+        return (
+          <Item
+            key={item.id}
+            item={item}
+            handleDeleteItem={handleDeleteItem}
+            handleToggleItem={handleToggleItem}
+          />
+        );
       })}
     </ul>
   );
